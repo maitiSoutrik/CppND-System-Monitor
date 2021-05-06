57474717e9bf3f9f5fd526a40e9e2fb30b84586a
maitiSoutrik <soutrikmaiti.uta@gmail.com>
2021-05-06 04:28:36 +0000

    Need to debug Format.h & Format.cpp

--- include/format.h
+++ include/format.h
@@ -4,7 +4,9 @@
 #include <string>
 
 namespace Format {
-std::string ElapsedTime(long times);  // TODO: See src/format.cpp
-};                                    // namespace Format
+std::string ElapsedTime(long times);   // TODO: See src/format.cp };                                    // namespace Format
+// Check to see if calculated number is smaller than 10
+// and return 0x for number smaller than 10 otherwise return the number
+std::string TimeValueToString(long time);
 
 #endif
--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,26 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long times) { 
+  string time = "00:00:00";
+  long cal = 0;
+  if(times > 0){
+  	// calculate hours
+    cal = times / 3600;
+    time = TimeValueToString(cal) + ":";
+    // calculate minutes
+    cal = (times / 60) % 60;
+    time += TimeValueToString(cal) + ":";
+    // calculate seconds
+    cal = times % 60;
+    time = TimeValueToString(cal) + ":";
+    
+  }
+  return time;
+}
+
+string Format::TimeValueToString(long time){
+	if(time < 10) {return "0" + std::to_string(time);}
+  	else {return std::to_string(time);}
+  	return " ";
+}
